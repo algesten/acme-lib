9d4702c986af394352bffb3deb4d7d647712caec
Martin Algesten <martin@algesten.se>
2021-08-20 14:32:56 +0200

    cargo fmt

--- src/persist.rs
+++ src/persist.rs
@@ -7,10 +7,10 @@ use std::collections::hash_map::{DefaultHasher, HashMap};
 use std::fs;
 use std::hash::{Hash, Hasher};
 use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
 #[cfg(unix)]
 use std::os::unix::fs::OpenOptionsExt;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex};
 
 use crate::{Error, Result};
 
@@ -147,15 +147,14 @@ impl Persist for FilePersist {
     fn put(&self, key: &PersistKey, value: &[u8]) -> Result<()> {
         let f_name = file_name_of(&self.dir, key);
         match key.kind {
-            PersistKind::AccountPrivateKey | PersistKind::PrivateKey =>
-                fs::OpenOptions::new()
-                 .mode(0o600)
-                 .write(true)
-                 .truncate(true)
-                 .create(true)
-                 .open(f_name)?
-                 .write_all(value)
-                 .map_err(Error::from),
+            PersistKind::AccountPrivateKey | PersistKind::PrivateKey => fs::OpenOptions::new()
+                .mode(0o600)
+                .write(true)
+                .truncate(true)
+                .create(true)
+                .open(f_name)?
+                .write_all(value)
+                .map_err(Error::from),
             PersistKind::Certificate => fs::write(f_name, value).map_err(Error::from),
         }
     }
