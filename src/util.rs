b817caade26133c9cda9973489d5e838cd38e1be
Thibaud Genty <thibaud.genty@cosmian.com>
2023-11-27 11:16:52 +0100

    Use NO_PAD for base64url

--- src/util.rs
+++ src/util.rs
@@ -5,7 +5,7 @@ use crate::req::req_safe_read_body;
 use crate::Result;
 
 pub(crate) fn base64url<T: ?Sized + AsRef<[u8]>>(input: &T) -> String {
-    base64::prelude::BASE64_URL_SAFE.encode(input)
+    base64::prelude::BASE64_URL_SAFE_NO_PAD.encode(input)
 }
 
 pub(crate) fn read_json<T: DeserializeOwned>(res: ureq::Response) -> Result<T> {
